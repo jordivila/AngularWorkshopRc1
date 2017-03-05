c63a021cf41dfbdb6767a052b2064f1841ba5f24
Jordi Vila <jordi.vila@gmail.com>
2017-03-05 17:13:04 +0100

    Excercise8_Redux2_Component_Interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AppStoreService } from './app-store.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app works!';
+  isLoading: boolean;
+
+  constructor(private appStore: AppStoreService) {
+    this.isLoading = false;
+  }
+
+  ngOnInit() {
+    this.appStore.subscribe(() => {
+      this.isLoading = this.appStore.getState().LoadingStore.loadingIsOpen;
+    });
+  }
+
 }
